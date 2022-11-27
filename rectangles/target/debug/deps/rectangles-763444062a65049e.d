fba2e19a5aeb152f82cf7ca2ab1c601f60ea2e33
MateusSartorio <mateussartorio39@gmail.com>
2022-11-26 23:52:22 -0300

    rectangles challenge completed and chapter 6

---
+++ rectangles/target/debug/deps/rectangles-763444062a65049e.d
@@ -0,0 +1,5 @@
+/home/mateus/Desktop/Projetos/Rust/rectangles/target/debug/deps/rectangles-763444062a65049e: src/main.rs
+
+/home/mateus/Desktop/Projetos/Rust/rectangles/target/debug/deps/rectangles-763444062a65049e.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/mateus/Desktop/Projetos/Rust/rectangles/target/debug/rectangles: /home/mateus/Desktop/Projetos/Rust/rectangles/src/main.rs
