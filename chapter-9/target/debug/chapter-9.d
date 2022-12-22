06c1492501cbf01804b9423ba2a34c4a7574e837
MateusSartorio <mateussartorio39@gmail.com>
2022-12-22 19:49:40 -0300

    chapter 8 started

---
+++ chapter-9/target/debug/chapter-9.d
@@ -0,0 +1,1 @@
+/home/mateus/Desktop/Projetos/rust-stuff/chapter-9/target/debug/chapter-9: /home/mateus/Desktop/Projetos/rust-stuff/chapter-9/src/main.rs
---
+++ chapter-9/target/debug/deps/chapter_9-7b43689ce095418c.d
@@ -0,0 +1,5 @@
+/home/mateus/Desktop/Projetos/rust-stuff/chapter-9/target/debug/deps/chapter_9-7b43689ce095418c: src/main.rs
+
+/home/mateus/Desktop/Projetos/rust-stuff/chapter-9/target/debug/deps/chapter_9-7b43689ce095418c.d: src/main.rs
+
+src/main.rs:
