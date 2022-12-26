e7de1af1a4cd85cd99893d91cad28c92da9befe1
MateusSartorio <mateussartorio39@gmail.com>
2022-12-26 00:05:38 -0300

    chapter 11 started

---
+++ adder/target/debug/deps/adder-424067c9fe475bf0.d
@@ -0,0 +1,5 @@
+/home/manoel/Desktop/Projetos/rust-stuff/adder/target/debug/deps/adder-424067c9fe475bf0: src/lib.rs
+
+/home/manoel/Desktop/Projetos/rust-stuff/adder/target/debug/deps/adder-424067c9fe475bf0.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-ba1d92ffb83c593c.d
@@ -0,0 +1,7 @@
+/home/manoel/Desktop/Projetos/rust-stuff/adder/target/debug/deps/adder-ba1d92ffb83c593c.rmeta: src/lib.rs
+
+/home/manoel/Desktop/Projetos/rust-stuff/adder/target/debug/deps/libadder-ba1d92ffb83c593c.rlib: src/lib.rs
+
+/home/manoel/Desktop/Projetos/rust-stuff/adder/target/debug/deps/adder-ba1d92ffb83c593c.d: src/lib.rs
+
+src/lib.rs:
