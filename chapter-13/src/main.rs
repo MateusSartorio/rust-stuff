70cc6e3d9d96405110821ba54aa5295bb0d0bd44
MateusSartorio <mateussartorio39@gmail.com>
2022-12-28 23:09:36 -0300

    closures done

--- chapter-13/src/main.rs
+++ chapter-13/src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -33,7 +35,35 @@ impl Inventory {
     }
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
+    let mut list = [
+        Rectangle { width: 10, height: 10 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+    
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {} operations", list, num_sort_operations);
+}
+
+/*
+fn main() {
+    let _expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor:: Blue],
     };
@@ -45,4 +75,49 @@ fn main() {
     let user_pref2 = None;
     let giveaway2 = store.giveaway(user_pref2);
     println!("The user with preference {:?} gets {:?}", user_pref2, giveaway2);
+
+    let example_closure = |x| x;
+    let _s = example_closure(String::from("hello"));
+    //let _n = example_closure(5);
 }
+*/
+
+/*
+fn main() {
+    let list = vec![1, 2, 3];
+    println!("Before defining the closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+}
+*/
+
+/*
+fn main() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining the closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+    
+    println!("{:?}", list);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+}
+*/
+
+/*
+fn main() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    println!("{:?}", list);
+}
+*/
